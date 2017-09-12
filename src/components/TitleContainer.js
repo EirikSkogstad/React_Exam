a8647472c3c35dbe12b8e582184ae19c25e71110
zenix <ze9ix@live.no>
2017-09-12 16:16:33 +0200

    title container component

--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -1,14 +1,28 @@
-import React, {Component} from 'react';
-import './title_container.css';
+import React, { Component } from "react";
+import "./title_container.css";
 
 class TitleContainer extends Component {
-    render() {
-        return (
-            <div className="TitleContainer-wrapper">
-
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      animals: ["cats", "honey badgers", "mongeese", "monkeys", "donkeys"]
+    };
+  }
+  render() {
+    return (
+      <div className="title-container-wrapper">
+        <h1>Movie list</h1>
+        <h4>
+          This page was coded by{" "}
+          {
+            this.state.animals[
+              Math.floor(Math.random() * this.state.animals.length)
+            ]
+          }
+        </h4>
+      </div>
+    );
+  }
 }
 
 export default TitleContainer;

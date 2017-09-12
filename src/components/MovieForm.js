46c247e84dcfd73e4e038df8038a642e197a92af
zenix <ze9ix@live.no>
2017-09-12 16:47:27 +0200

    Can now click to show other animals('contributors')

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,14 +1,29 @@
-import React, {Component} from 'react';
-import './movie_form.css';
+import React, { Component } from "react";
+import "./movie_form.css";
 
 class MovieForm extends Component {
-    render() {
-        return (
-            <div className="MovieForm-wrapper">
+  render() {
+    return (
+      <div className="movie-form-wrapper container-fluid">
+        <form>
+          <label htmlFor="input1">
+            Title:
+            <input type="text" id="input1" />
+          </label>
+          <label htmlFor="input2">
+            Year:
+            <input type="text" id="input2" />
+          </label>
+          <label htmlFor="input3">
+            Description:
+            <input type="text" id="input3" />
+          </label>
 
-            </div>
-        );
-    }
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default MovieForm;
--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -5,23 +5,44 @@ class TitleContainer extends Component {
   constructor() {
     super();
     this.state = {
-      animals: ["cats", "honey badgers", "mongeese", "monkeys", "donkeys"]
+      animals: [
+        "cats",
+        "honey badgers",
+        "mongeese",
+        "monkeys",
+        "donkeys",
+        "snakes",
+        "'Vampire'-kittens"
+      ],
+      currentAnimal: ""
     };
+    this.changeAnimal = this.changeAnimal.bind(this);
   }
+
+  componentWillMount() {
+    this.changeAnimal();
+  }
+
   render() {
     return (
       <div className="title-container-wrapper">
         <h1>Movie list</h1>
-        <h4>
-          This page was coded by{" "}
-          {
-            this.state.animals[
-              Math.floor(Math.random() * this.state.animals.length)
-            ]
-          }
-        </h4>
+        <h4>This page was coded by {this.state.currentAnimal}</h4>
+
+        <button onClick={this.changeAnimal}>View other contributors..</button>
       </div>
     );
+  }
+
+  changeAnimal() {
+    let nextAnimal = this.state.currentAnimal;
+    do {
+      nextAnimal = this.state.animals[
+        Math.floor(Math.random() * this.state.animals.length)
+      ];
+    } while (nextAnimal === this.state.currentAnimal);
+
+    this.setState({ currentAnimal: nextAnimal });
   }
 }
 

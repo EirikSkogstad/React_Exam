0a04ae11c3931a894f6f12f43a97f908ef4f3408
Zenix <ze9ix@live.no>
2017-09-13 18:47:08 +0200

    work on server

--- _backend/server.js
+++ _backend/server.js
@@ -1,11 +1,31 @@
+// Imports:
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const serverPort = 1234;
 const dbName = 'movies';
 
+// Mongoose config:
 mongoose.connect(`mongodb://localhost/${dbName}/`);
+const movieSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    date: {type: Date, required: true},
+    description: {type: String, required: true}
+});
+const movieModel = mongoose.model('Movie', movieSchema);
+
+
 
 app.get('/movies', (req, res) => {
-    res.send()
+    movieModel.find((result, err) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(result);
+        }
+    });
 });
+
+app.listen(serverPort, () => console.log(`Listening on port: ${serverPort}`));

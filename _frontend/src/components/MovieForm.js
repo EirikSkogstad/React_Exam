df550f10bf575000ddd6202d4fe1e3c9eec48270
Zenix <ze9ix@live.no>
2017-09-14 15:28:19 +0200

    added form validation

--- _frontend/src/components/MovieForm.js
+++ _frontend/src/components/MovieForm.js
@@ -1,29 +1,69 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./movie_form.css";
 
 class MovieForm extends Component {
-  render() {
-    return (
-      <div className="movie-form-wrapper">
-        <form>
-          <label htmlFor="input1">
-            Title:
-            <input type="text" id="input1" />
-          </label>
-          <label htmlFor="input2">
-            Year:
-            <input type="text" id="input2" />
-          </label>
-          <label htmlFor="input3">
-            Description:
-            <textarea type="textbox" id="input3" />
-          </label>
 
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            maxYear: new Date().getFullYear(),
+            minYear: 1800,
+            Name: "",
+            Year: "",
+            Description: ""
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="movie-form-wrapper">
+                <form>
+                    <label htmlFor="titleInput">
+                        Title:
+                        <input onChange={this.handleInputChange} type="text" name="Title" id="titleInput" required/>
+                    </label>
+                    <label htmlFor="yearInput">
+                        Year:
+                        <input onChange={this.handleInputChange} type="number" name="Year" min={this.state.minYear}
+                               max={this.state.maxYear} id="yearInput" required/>
+                    </label>
+                    <label htmlFor="descriptionInput">
+                        Description:
+                        <textarea onChange={this.handleInputChange} type="textbox" name="Description"
+                                  id="descriptionInput" required/>
+                    </label>
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+
+    handleInputChange(event) {
+        const value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({
+            name: value
+        })
+    }
+
+    handleSubmit(event) {
+        if (this.isFormFilled()) {
+
+        }
+    }
+
+    isFormFilled() {
+        if(this.state.Name === "") {
+            return false;
+        }
+        if(this.state.Year === "" ) {
+            return false;
+        }
+        return this.state.Description !== "";
+    }
 }
 
 export default MovieForm;

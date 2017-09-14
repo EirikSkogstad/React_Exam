ab5513816e018649b7c65c1510f955f2333f6c05
Zenix <ze9ix@live.no>
2017-09-14 15:52:22 +0200

    reset form after use

--- _frontend/src/components/MovieForm.js
+++ _frontend/src/components/MovieForm.js
@@ -2,13 +2,12 @@ import React, {Component} from "react";
 import "./movie_form.css";
 
 class MovieForm extends Component {
-
     constructor() {
         super();
         this.state = {
             maxYear: new Date().getFullYear(),
             minYear: 1800,
-            Name: "",
+            Title: "",
             Year: "",
             Description: ""
         };
@@ -19,20 +18,39 @@ class MovieForm extends Component {
     render() {
         return (
             <div className="movie-form-wrapper">
-                <form>
+                <form onSubmit={this.handleSubmit}
+                      ref={(ref) => this.formRef = ref}>
                     <label htmlFor="titleInput">
                         Title:
-                        <input onChange={this.handleInputChange} type="text" name="Title" id="titleInput" required/>
+                        <input
+                            onChange={this.handleInputChange}
+                            type="text"
+                            name="Title"
+                            id="titleInput"
+                            required
+                        />
                     </label>
                     <label htmlFor="yearInput">
                         Year:
-                        <input onChange={this.handleInputChange} type="number" name="Year" min={this.state.minYear}
-                               max={this.state.maxYear} id="yearInput" required/>
+                        <input
+                            onChange={this.handleInputChange}
+                            type="number"
+                            name="Year"
+                            min={this.state.minYear}
+                            max={this.state.maxYear}
+                            id="yearInput"
+                            required
+                        />
                     </label>
                     <label htmlFor="descriptionInput">
                         Description:
-                        <textarea onChange={this.handleInputChange} type="textbox" name="Description"
-                                  id="descriptionInput" required/>
+                        <textarea
+                            onChange={this.handleInputChange}
+                            type="textbox"
+                            name="Description"
+                            id="descriptionInput"
+                            required
+                        />
                     </label>
                     <button>Submit</button>
                 </form>
@@ -45,24 +63,42 @@ class MovieForm extends Component {
         const name = event.target.name;
 
         this.setState({
-            name: value
-        })
+            [name]: value
+        });
     }
 
     handleSubmit(event) {
-        if (this.isFormFilled()) {
+        event.preventDefault();
 
+        if (this.isFormFilled()) {
+            alert("Sending form!");
         }
+        else {
+            alert("Form is not filled properly out!");
+        }
+
+        this.resetForm();
     }
 
     isFormFilled() {
-        if(this.state.Name === "") {
+        if (this.state.Title === "") {
             return false;
         }
-        if(this.state.Year === "" ) {
+        if (this.state.Year === "") {
             return false;
         }
         return this.state.Description !== "";
+    }
+
+    resetForm() {
+        this.formRef.reset();
+        this.setState(
+            {
+                Title: "",
+                Year: "",
+                Description: "",
+            }
+        )
     }
 }
 
